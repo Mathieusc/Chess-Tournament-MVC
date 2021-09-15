4250c7c8f49606c4c96188c35b35b65ef3fe03a0
Mathieusc <62360534+Mathieusc@users.noreply.github.com>
2021-09-15 10:27:46 +0200

    Create test_algo.py

---
+++ test_algo.py
@@ -0,0 +1,40 @@
+import itertools
+
+played = [("Magnus", "Alexandra"), ("Yifan", "Magnus"), ("Levron", "Anish"), ("Wenjun", "Koneru")]
+
+joueurs = ["Magnus", "Alexandra", "Yifan", "Ding", "Levron", "Anish", "Wenjun", "Koneru"]
+
+
+def test(joueurs, played):
+    match = []
+    print("Liste de joueurs avant début fonction: ")
+    print(joueurs)
+    for player1, player2 in zip(itertools.repeat(joueurs[0]), joueurs[1:]):
+        print(player1, player2)
+        if (player1, player2) not in played and (player2, player1) not in played:
+            match = (player1, player2)
+            print("NEXT MATCH")
+            print(match)
+            # Supprimer les joueurs de ma liste trié
+            joueurs.remove(player1)
+            joueurs.remove(player2)
+            print("List de joueurs après remove: ")
+            print(joueurs)
+            # Ajouter le match dans la liste déjà joués
+            played.append(match)
+            print("Matchs déjà joués:")
+            print(played)
+            # Comment itérer sur les autres joueurs ?
+            break
+    # try:
+    #     test(joueurs, played)
+    # except IndexError:
+    #     print("Fin de liste")
+    # return match
+
+w = test(joueurs, played)
+print("test")
+print(w)
+
+
+
